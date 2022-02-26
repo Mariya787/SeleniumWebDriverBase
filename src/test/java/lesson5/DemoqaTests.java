03fac1a77889f8abbfccfbd32c91408d71ca4a5c
Mariya Korzhavina <m.s.korzhavina@gmail.com>
2022-02-26 19:30:34 +0700

    Assertions added

--- src/test/java/lesson5/DemoqaTests.java
+++ src/test/java/lesson5/DemoqaTests.java
@@ -1,6 +1,7 @@
 package lesson5;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,11 +12,13 @@ import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.opera.OperaDriver;
 import org.openqa.selenium.support.ui.Select;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
 
 public class DemoqaTests {
 
     private WebDriver driver;
+    private File file;
 
 
     @BeforeEach
@@ -37,7 +40,7 @@ public class DemoqaTests {
         driver.get("https://demoqa.com/automation-practice-form");
         driver.manage().window().maximize();
 
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
     }
 
     @Test
@@ -61,8 +64,17 @@ public class DemoqaTests {
         String firstName = "Mariya";
         String lastName = "Korzhavina";
         String email = "m.s.korzhavina@gmail.com";
-        String mobile = "9529051494";
+        String gender = "Female";
+        String mobile = "9529061505";
+        String birthday = "01 July,1987";
         String address = "Kolcovo 36";
+        String subjectFirst = "English";
+        String subjectSecond = "Maths";
+        String hobbyFirst = "Reading";
+        String hobbySecond = "Music";
+        String picture = "picture.jpg";
+        String studentsState = "NCR";
+        String studentsCity = "Delhi";
 
 
         driver.findElement(By.id("firstName")).sendKeys(firstName);
@@ -80,44 +92,54 @@ public class DemoqaTests {
         Select selectMonth = new Select(month);
         selectYear.selectByValue("1987");
         selectMonth.selectByValue("6");
-
         driver.findElement(By.xpath("//*[@id=\"dateOfBirth\"]//div[@aria-label='Choose Wednesday, July 1st, 1987']")).click();
 
         WebElement subjects = driver.findElement(By.id("subjectsInput"));
-        Thread.sleep(2000);
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", subjects);
-        Thread.sleep(2000);
-        subjects.sendKeys("English");
+        subjects.sendKeys(subjectFirst);
         subjects.sendKeys(Keys.ENTER);
-        subjects.sendKeys("Maths");
+        subjects.sendKeys(subjectSecond);
         subjects.sendKeys(Keys.ENTER);
 
         driver.findElement(By.xpath("//*[@id=\"hobbiesWrapper\"]/div[2]/div[2]/label")).click();
         driver.findElement(By.cssSelector("[for='hobbies-checkbox-3']")).click();
 
-        driver.findElement(By.id("uploadPicture")).sendKeys("C:\\Users\\Маша\\IdeaProjects\\SeleniumWebDriverBase\\src\\picture.jpg");
+        file = new File("src/picture.jpg");
+        WebElement selectPictureButton = driver.findElement(By.id("uploadPicture"));
+        selectPictureButton.sendKeys(file.getAbsolutePath());
 
         driver.findElement(By.id("currentAddress")).sendKeys(address);
 
-        Thread.sleep(2000);
-
         WebElement state = driver.findElement(By.id("react-select-3-input"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", state);
-        Thread.sleep(2000);
-        state.sendKeys("NCR");
+        state.sendKeys(studentsState);
         state.sendKeys(Keys.ENTER);
 
         WebElement city = driver.findElement(By.id("react-select-4-input"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", city);
-        Thread.sleep(2000);
-        city.sendKeys("Delhi");
+        city.sendKeys(studentsCity);
         city.sendKeys(Keys.ENTER);
 
-        Thread.sleep(2000);
+        WebElement submitButton = driver.findElement(By.id("submit"));
+        //((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", submitButton);
+        submitButton.sendKeys(Keys.ENTER);
+        Thread.sleep(3000);
 
-        driver.findElement(By.id("submit")).click();
+        SoftAssertions softAssert = new SoftAssertions();
 
-        Thread.sleep(5000);
+        //softAssert.assertThat(driver.findElement(By.id("example-modal-sizes-title-lg")).getText()).isEqualTo("Thanks for submitting the form");
+        softAssert.assertThat(driver.findElement(By.xpath("//tbody/tr[1]/td[2]")).getText()).isEqualTo(firstName + " "+ lastName);
+        softAssert.assertThat(driver.findElement(By.xpath("//tbody/tr[2]/td[2]")).getText()).isEqualTo(email);
+        softAssert.assertThat(driver.findElement(By.xpath("//tbody/tr[3]/td[2]")).getText()).isEqualTo(gender);
+        softAssert.assertThat(driver.findElement(By.xpath("//tbody/tr[4]/td[2]")).getText()).isEqualTo(mobile);
+        softAssert.assertThat(driver.findElement(By.xpath("//tbody/tr[5]/td[2]")).getText()).isEqualTo(birthday);
+        softAssert.assertThat(driver.findElement(By.xpath("//tbody/tr[6]/td[2]")).getText()).isEqualTo(subjectFirst+", "+subjectSecond);
+        softAssert.assertThat(driver.findElement(By.xpath("//tbody/tr[7]/td[2]")).getText()).isEqualTo(hobbyFirst+", "+hobbySecond);
+        softAssert.assertThat(driver.findElement(By.xpath("//tbody/tr[8]/td[2]")).getText()).isEqualTo(picture);
+        softAssert.assertThat(driver.findElement(By.xpath("//tbody/tr[9]/td[2]")).getText()).isEqualTo(address);
+        softAssert.assertThat(driver.findElement(By.xpath("//tbody/tr[10]/td[2]")).getText()).isEqualTo(studentsState+" "+studentsCity);
+
+        softAssert.assertAll();
 
     }
 
